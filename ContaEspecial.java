6c9010e90d31c676e38533996fbc1dd7b0240cbf
alanabclins <alanacamaralins@gmail.com>
2023-03-30 13:18:27 -0300

    Commit

---
+++ ContaEspecial.java
@@ -0,0 +1,25 @@
+public class ContaEspecial extends SistemaBanco {
+     protected double limite;
+
+    public ContaEspecial(int agencia, int numDaConta, String nome, int cpf, String endereco, double saldo,
+            double limite) {
+        super(agencia, numDaConta, nome, cpf, endereco, saldo);
+        this.limite = 100;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    
+     public double saqueEspecial (double value){
+        this.saldo= saldo -value;
+        if(this.saldo<saldo-100){
+            return -1;
+        }
+       return saldo;
+    }
+}
---
+++ poupanca.java
@@ -0,0 +1,24 @@
+public class poupanca extends SistemaBanco{
+
+    protected int diaRend;
+    
+    public double atualiza (int rendimento){
+        return this.saldo*rendimento;
+    }
+
+    public int getDiaRend() {
+        return diaRend;
+    }
+
+    public void setDiaRend(int diaRend) {
+        this.diaRend = diaRend;
+    }
+
+    public poupanca(int agencia, int numDaConta, String nome, int cpf, String endereco, double saldo, int diaRend) {
+        super(agencia, numDaConta, nome, cpf, endereco, saldo);
+        this.diaRend = diaRend;
+    }
+     
+    
+    }
+    
